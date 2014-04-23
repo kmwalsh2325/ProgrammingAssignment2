77e66ec1caed42ab3fb812089ec403d593bf7d4e
kmwalsh2325 <kmwalsh@mtu.edu>
2014-04-23 11:45:36 -0400

    First go at a solution for caching a matrix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a non-singular matrix and stores the inverse if it already exists
+## if the inverse does not exist cacheSolve will compute the inverse
+## and store it or cache it
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix establishes a list of functions for a matrix, x
+## ARGS:
+## 	x: a non-singular matrix
+## RETURNS a list of functions pertaining to the values of the matrix
+## and its inverse
 
-}
+	n <- NULL
+
+	# set the value of the matrix
+	set <- function(z) {
+		x <<- z
+		n <<- NULL
+	}
+
+	# get the value of the matrix
+	get <- function() x
+
+	# set the value of the inverse
+	setinverse <- function(inverse) n <<- inverse
+
+	# get the value of the inverse
+	getinverse <- function() n
+
+	# return list of these functions
+	list(set = set, get = get,
+		 setinverse = setinverse,
+		 getinverse = getinverse)
+
+} ## end makeCacheMatrix
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## cacheSolve computes or gets the inverse of the matrix provided
+## ARGS:
+## 	x: a non-singular matrix
+## RETURNS the inverse of x
+
+	# attempt to get the inverse 
+    n <- x$getinverse()
+
+	# check for its existence and return if it does exist
+    if(!is.null(n)) {
+    	message("getting cached inverse")
+    	return(n)
+    }
+    else {
+    # compute the inverse of the matrix
+    matrix <- x$get()
+    n <- round(solve(matrix, ...), 3)
+    x$setinverse(n)
+    n
+	}
+
+} ## end cacheSolve
